4c732d96596e58ba1f1a50a8d8c4540fa1286eba
Joachim Falk <joachim.falk@gmx.de>
2012-05-27 18:31:28 +0200

    On the way to 3.0 (quilt) source format

--- fetch-subprojects.sh
+++ fetch-subprojects.sh
@@ -3,13 +3,12 @@
 cd `dirname "$0"` || \
   { echo "WHAT?! Can't change to script directory!"; exit -1; }
 
-TIGERVNC_VERSION=$(basename $(readlink tigervnc))
-
-echo "Fetching debian xorg source package..."
+echo "Fetching subprojects xorg and tigervnc source packages..."
 git submodule update --init || \
-  { echo "Ups... Can't fetch debian xorg source package!"; exit -1; }
-echo "Fetching ${TIGERVNC_VERSION} tigervnc sources..."
-test -d tigervnc-tags || mkdir tigervnc-tags || \
-  { echo "Ups.. Can't create tigervnc-tags subdir!"; exit -1; }
-svn co http://tigervnc.svn.sourceforge.net/svnroot/tigervnc/tags/${TIGERVNC_VERSION} tigervnc-tags/${TIGERVNC_VERSION} || \
-  { echo "Ups.. Can't fetch ${TIGERVNC_VERSION} tigervnc sources!"; exit -1; }
+  { echo "Ups... Can't fetch subprojects!"; exit -1; }
+#TIGERVNC_VERSION=$(basename $(readlink tigervnc))
+#echo "Fetching ${TIGERVNC_VERSION} tigervnc sources..."
+#test -d tigervnc-tags || mkdir tigervnc-tags || \
+#  { echo "Ups.. Can't create tigervnc-tags subdir!"; exit -1; }
+#svn co http://tigervnc.svn.sourceforge.net/svnroot/tigervnc/tags/${TIGERVNC_VERSION} tigervnc-tags/${TIGERVNC_VERSION} || \
+#  { echo "Ups.. Can't fetch ${TIGERVNC_VERSION} tigervnc sources!"; exit -1; }
